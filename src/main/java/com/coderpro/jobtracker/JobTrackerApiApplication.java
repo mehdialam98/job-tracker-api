ee6fd949dbd9c35958c549d64c0f51a1083df26f
mehdialam98 <mahadi.alam76@gmail.com>
2026-06-14 13:31:16 -0400

    Milestone 0: generate Spring Boot project with Spring Web

---
+++ src/main/java/com/coderpro/jobtracker/JobTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.coderpro.jobtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobTrackerApiApplication.class, args);
+	}
+
+}
